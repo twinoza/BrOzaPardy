1e86fe9e4cc973629a318370f0963df1ae5ebfd4
Neal Oza <neal.n.oza@gmail.com>
2016-12-20 14:44:32 -0800

    Add .py files for classes, main, and importing data from TSv

---
+++ importOzaPardy.py
@@ -0,0 +1,57 @@
+from numpy import loadtxt
+from math import ceil
+
+#Pass in Boxes to be populated
+def getJeopardyData(boxes, gameType=0, doc_name="OzaPardy"):
+    print("Start getJeopardyData")
+
+    if gameType == 0:
+        fName = 'static/data/OzaPardy - Single.tsv'
+        data = loadtxt(fName, dtype='S', delimiter='\t', skiprows=1)
+        print('Loaded Single Jeopardy')
+    elif gameType == 1:
+        fName = 'static/data/OzaPardy - Single.tsv'
+        data = loadtxt(fName, dtype='S', delimiter='\t', skiprows=1)
+        print('Loaded Double Jeopardy')
+    else:
+        print('Error in getJeopardyData: Invalid gameType')
+        return()
+    
+    for rowNum, row in enumerate(data):
+        for colNum, boxVal in enumerate(row):
+            boxNum = int((ceil(rowNum/2.)*6) + (colNum-1))
+            if colNum != 0:
+                if rowNum == 0:   # Fill in category Names
+                    ###boxes[gameType][boxNum] = boxVal #row[key].text
+                    print(boxVal)
+                else:         # Fill in Clue/Response boxes
+                    parseOzaPardyBox(self.boxes[gameType][boxNum], boxVal, row[0])
+
+# opBox is OzaPardyBox
+# OLD def parseOzaPardyBox(self, opBox, key, gDict):
+# cellData is the first column of the spreadsheet which determines whether
+#     the row is a clue or a response and the point value associated with 
+#     that row.
+def parseOzaPardyBox(opBox, boxVal, cellData):
+    #[cellType, cellVal] = gDict['tiles'].text.split()
+    [cellType, cellVal] = cellData.split()
+    opBox.value = cellVal
+
+    parsedMediaType = ''
+    clue = boxVal
+
+    if '<>' in boxVal:
+        parsedMediaType, parsedMediaFName, clue = boxVal.split('<>')
+    if cellType=="Clue":
+        if parsedMediaType == 'Img':
+            opBox.mediaType = 1
+            opBox.mediaFName = mediaDir + parsedMediaFName
+        elif parsedMediaType == 'Aud':
+            opBox.mediaType = 2
+            opBox.mediaFName = mediaDir + parsedMediaFName
+        elif parsedMediaType == 'Vid':
+            opBox.mediaType = 3
+            opBox.mediaFName = mediaDir + parsedMediaFName
+        opBox.clue = self.myWrap(clue)
+    else:
+        opBox.response = self.myWrap(clue)
