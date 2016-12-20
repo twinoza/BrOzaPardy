81355537ab835a3f9ddc32758ba939b574258e99
Neal Oza <neal.n.oza@gmail.com>
2016-12-20 15:05:26 -0800

    Merged Neal and Dad's main.py

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 from flask import Flask, render_template
+import logging
+import ozaPardyClasses as opc
+
 app = Flask(__name__)
-temp = 10
+temp = 0
+# Initialize the boxes for OzaPardy
+
+Team1 = opc.team('Mice', 2500)
+Team2 = opc.team('Men', 2200)
+Teams = [Team1, Team2]
 
 @app.route('/')
 def index():
-    return render_template('index.html', timer = "0:15")
-
-@app.route('/blank')
-def blank(this):
-	this.text = "that"
-	return render_template('board.html')
+    global Teams
+    timer = "0:15"
+    Teams[1].score += 100
+    return render_template('index.html', **locals())
 
 @app.route('/board/<sd>')
 def drawBoard(sd):
-	cats = ["This & That", "That&This", "This", "That", "Everything", "Lots & Lots of Nothing Else"]
-	gmMulti = 1
-	if sd == "d":
-		gmMulti = 2
-	return render_template( 'board.html', **locals() )
+    cats = ["This & That", "That&This", "This", "That", "Everything", "Lots & Lots of Nothing Else"]
+    gmMulti = 1
+    if sd == "d":
+        gmMulti = 2
+    return render_template( 'board.html', **locals() )
 
 @app.route('/home')
 def drawHome():
-	global temp
-	temp = temp + 1
-	return render_template('home.html', number = temp)
+    global temp
+    temp = temp + 1
+    return render_template('home.html', number = temp)
 
 if __name__ == '__main__':
     app.run()
