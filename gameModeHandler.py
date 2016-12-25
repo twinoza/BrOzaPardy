17c2adc90e2f70c04ae14dcc5f17d0b9b55969b9
Danny Broberg <dbroberg@berkeley.edu>
2016-12-24 16:49:58 -0800

    add back answer/clue time vars

--- gameModeHandler.py
+++ gameModeHandler.py
@@ -6,6 +6,9 @@ import time
 import g
 
 
+clueShownTime = 15.
+clueAnsTime = 15.
+
 def arduinoQuestionQueryThingFunction():
     at = arduinoTalker()
     at.checkup() #wait until buttons are down to continue
@@ -13,7 +16,7 @@ def arduinoQuestionQueryThingFunction():
 
     clueHappening = True
     while clueHappening:
-        val = at.contcheckforanswer()
+        val = at.contcheckforanswer(clueShownTime)
         if val=='0':
             print('Clue not answered in time. Moving on...')
             clueHappening=False
@@ -29,8 +32,10 @@ def arduinoQuestionQueryThingFunction():
                 #NEAL:   score positive points for that team
                 #NEAL:   set clueIsShown = False
                 #NEAL:elif answered incorrectly:
-                #NEAL:   deduct points from the team, added clue back to screen 
-                print('hey')
+                #NEAL:   deduct points from the team, added clue back to screen
+                #NOTE: THERE at end of timeout, there should be an option for referee
+                #       to say correct or incorrect (so that points are not automatically deducted)
+                pass
 
     return
 
